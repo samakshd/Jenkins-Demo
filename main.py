ec39e9dd0ad81b25bbb0f97c2e865727cbff87ee
Samaksh Dhingra <55679277+samakshd@users.noreply.github.com>
2021-11-23 13:07:19 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,14 @@
+def addition(a,b):
+  return a+b
+
+def subtraction(a,b):
+  return a-b
+
+def multiplication(a,b):
+  return a*b
+
+def quotient(a,b):
+  return a/b
+    
+def remainder(a,b):
+  return a%b
---
+++ test.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+# Test file for basic arithmetic operations on two numbers
+
+import unittest
+from program import mean
+
+class TestMean(unittest.TestCase):
+    def test_addition(self):
+        """
+        Test case 1 for checking addition operation on given two numbers
+        """
+        data = [5,6]
+        result = addition(data)
+        self.assertEqual(result, 11)
+        
+    def test_subtraction(self):
+        """
+        Test case 2 for checking subtraction operation on given two numbers
+        """
+        data = [5,6]
+        result = subtraction(data)
+        self.assertEqual(result, -1)
+    
+    def test_multiplication(self):
+        """
+        Test case 3 for checking multiplication operation on given two numbers
+        """
+        data = [5,6]
+        result = multiplication(data)
+        self.assertEqual(result, 30)
+    
+    def test_quotient(self):
+        """
+        Test case 4 for checking quotient operation on given two numbers
+        """
+        data = [5,6]
+        result = quotient(data)
+        self.assertEqual(result, 1)
+        
+    def test_remainder(self):
+        """
+        Test case 5 for checking remainder operation on given two numbers
+        """
+        data = [5,6]
+        result = remainder(data)
+        self.assertEqual(result, 1)
+
+    
+if __name__ == '__main__':
+    unittest.main()
